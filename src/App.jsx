6035ea9cc8d5dac14e56004316b027786bbea41b
Hardik <prajapatihardik375@gmail.com>
2025-11-09 17:49:25 +0530

    remove <a> tag and add Link tag

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { HomePage } from './Pages/HomePage'
 import { Checkout } from './Pages/Checkout'
+import { OrdersPage } from './Pages/OrdersPage'
+import { Tracking } from './Pages/Tracking'
+
+
 
 
 
@@ -13,6 +17,8 @@ function App() {
       <Routes>
         <Route path='/' element={ < HomePage />} /> 
         <Route path='/checkout' element={ < Checkout/> } /> 
+        <Route path='/orders' element={ < OrdersPage/> } /> 
+        <Route path='/tracking' element={ < Tracking/> } /> 
       </Routes>
             
     </>
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -15,12 +15,12 @@ export const HomePage = ()=>{
 
     <div className="header">
       <div className="left-section">
-        <a href="/" className="header-link">
+        <Link to="/" className="header-link">
           <img className="logo"
             src="images/logo-white.png" />
           <img className="mobile-logo"
             src="images/mobile-logo-white.png" />
-        </a>
+        </Link>
       </div>
 
       <div className="middle-section">
@@ -32,16 +32,16 @@ export const HomePage = ()=>{
       </div>
 
       <div className="right-section">
-        <a className="orders-link header-link" href="orders.html">
+        <Link className="orders-link header-link" to="/orders">
 
           <span className="orders-text">Orders</span>
-        </a>
+        </Link>
 
-        <Link to="/checkout" >
+        <Link className="cart-link header-link" to="/checkout">   
           <img className="cart-icon" src="images/icons/cart-icon.png" />
           <div className="cart-quantity">3</div>
           <div className="cart-text">Cart</div>
-        </Link>  
+        </Link>
       </div>
     </div>
 
